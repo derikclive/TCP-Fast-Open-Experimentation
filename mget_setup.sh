b8ce277cac08e5a2cdaeba2732ef0861c3db4282
derikclive <derikclive@gmail.com>
2017-10-21 19:42:23 +0530

    Finished setting up mget but had some problems setting up dummynet

---
+++ mget_setup.sh
@@ -0,0 +1,18 @@
+sudo apt-get -y install autoconf autogen automake libtool gtk-doc-tools xsltproc\
+ gettext zlib1g-dev liblzma-dev libbz2-dev libgnutls-dev libidn2-0-dev flex \
+ libssl-dev
+
+ cd ~;
+git clone http://github.com/rockdaboot/mget
+
+cd mget
+
+./autogen.sh
+
+./configure
+
+make
+
+sudo make install
+
+touch ~/.mgetrc
