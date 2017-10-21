4f3ffaa2414b50ad870e90bc8ef2e1d349aac56b
derikclive <derikclive@gmail.com>
2017-10-22 00:25:43 +0530

    Fixed a error in mget installtion

--- mget_setup.sh
+++ mget_setup.sh
@@ -1,18 +1,14 @@
-sudo apt-get -y install autoconf autogen automake libtool gtk-doc-tools xsltproc\
+dir=`pwd`
+
+apt-get -y install autoconf autogen automake libtool gtk-doc-tools xsltproc\
  gettext zlib1g-dev liblzma-dev libbz2-dev libgnutls-dev libidn2-0-dev flex \
  libssl-dev
-
- cd ~;
+cd ~
+rm -rf mget
 git clone http://github.com/rockdaboot/mget
-
 cd mget
-
 ./autogen.sh
-
 ./configure
-
 make
-
-sudo make install
-
+make install
 touch ~/.mgetrc
--- run.py
+++ run.py
@@ -4,8 +4,11 @@ def configure(cmd):
     subprocess.Popen(cmd, shell=True).communicate()
 
 if __name__ == '__main__':
-    configure('chmod +x mget_setup.sh')
+   
+    subprocess.call(["chmod", "+x" , "mget_setup.sh"])
+    configure("sudo ./mget_setup.sh")
+
     configure('chmod +x mininet_setup.sh')
-    configure('sudo ./mininet_setup.sh')
+ 
     configure('sudo ./mget_setup.sh')
     configure('sudo ./mininet_setup.sh') # Given in reproducing networks page to run this twice.
