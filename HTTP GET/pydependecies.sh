73a96761bb540de4457fb92c515ca0d7c4160735
Aneesh <aneesh297@gmail.com>
2017-11-12 08:54:08 +0000

    pydependices.sh now working

--- HTTP GET/pydependecies.sh
+++ HTTP GET/pydependecies.sh
@@ -1,9 +1,12 @@
-sudo apt-get python-setuptools
-sudo apt-get python-dev 
+sudo apt-get -y install python-setuptools
+sudo apt-get -y install python-dev 
 sudo easy_install pip 
 pip install pip --upgrade 
 
-sudo apt-get numpy
-sudo apt-get build-dep python-matplotlib
+sudo apt-get -y install numpy
+sudo apt-get -y build-dep python-matplotlib
+sudo apt-get install python-matplotlib
+sudo pip install pandas
+sudo apt-get install python-pandas
 
-python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+sudo python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
